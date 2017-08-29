aba83c7772f89c5e9adaf79876e00e36d10c16bf
jennyla <laskavi@post.bgu.ac.il>
2017-08-29 13:30:55 +0300

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,19 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = '';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    return axios.get(requestUrl).then(function (res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      }else{
+        retunr res.data.main.temp;
+      }
+    },function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
