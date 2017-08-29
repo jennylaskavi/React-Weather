a83273a4e142995f63444be7affeeb5cf4934b58
jennyla <laskavi@post.bgu.ac.il>
2017-08-29 14:56:52 +0300

    Add foundation and jqury

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -5,12 +5,12 @@ const OPEN_WEATHER_MAP_URL = '';
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}`;
     return axios.get(requestUrl).then(function (res){
       if(res.data.cod && res.data.message){
         throw new Error(res.data.message);
       }else{
-        retunr res.data.main.temp;
+        return res.data.main.temp;
       }
     },function(res){
       throw new Error(res.data.message);
--- example-promise.js
+++ example-promise.js
@@ -1,25 +1,45 @@
-function getTempCallback (location, callback){
-callback(undefined, 78);
-callback('City not found');
-}
+// function getTempCallback (location, callback){
+// callback(undefined, 78);
+// callback('City not found');
+// }
+//
+// getTempCallback('Israel', function (err, temp){
+//   if(err){
+//     console.log('error', err);
+//   }else{
+//     console.log('success', temp);
+//   }
+// });
+//
+// function getTempPromise(location){
+//   return new Promise(function(resolve,reject){
+//     setTimeout(function (){
+//       resolve(79);
+//       reject('City not found');
+//     }, 1000);
+//
+//   });
+// }
+//
+// getTempPromise('Israel').then(function (temp){
+//   console.log('promise success', temp);
+// },function(err){
+//   console.log('promise error', err);
+// })
 
-getTempCallback('Israel', function (err, temp){
-  if(err){
-    console.log('error', err);
-  }else{
-    console.log('success', temp);
-  }
-});
 
-function getTempPromise(location){
-  return new Promise(function(resolve,reject){
-    resolve(79);
-    reject('City not found');
+function addPromise (a,b){
+  return new Promise(function (resolve, reject){
+    if(typeof a === 'number' && typeof b === 'number'){
+      resolve(a+b);
+    }else{
+      reject('A & B need to be numbers');
+    }
   });
 }
 
-getTempPromise('Israel').then(function (temp){
-  console.log('promise success', temp);
-},function(err){
-  console.log('promise error', err);
+addPromise(7,8).then(function (sum) {
+  console.log('success', sum);
+}, function(err){
+  console.log('error',err);
 })
